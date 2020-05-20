d3301b269c0060c6a5c34f279577ecdc33f06aaf
Stephen Jiang <398665255@qq.com>
2020-05-21 02:01:42 +0800

    Site updated: 2020-05-21 02:01:38

--- js/utils.js
+++ js/utils.js
@@ -53,7 +53,6 @@ NexT.utils = {
       link.rel = 'noopener external nofollow noreferrer';
       link.target = '_blank';
       link.className = element.className;
-      link.title = element.title;
       link.innerHTML = element.innerHTML;
       element.parentNode.replaceChild(link, element);
     });
@@ -64,18 +63,15 @@ NexT.utils = {
    */
   registerCopyCode: function() {
     document.querySelectorAll('figure.highlight').forEach(element => {
-      element.querySelectorAll('.code .line span').forEach(span => {
-        span.classList.forEach(name => {
-          span.classList.remove(name);
-          span.classList.add(`hljs-${name}`);
-        });
-      });
-      element.insertAdjacentHTML('beforeend', '<div class="copy-btn"><i class="fa fa-clipboard fa-fw"></i></div>');
-      const button = element.querySelector('.copy-btn');
+      const box = document.createElement('div');
+      element.wrap(box);
+      box.classList.add('highlight-container');
+      box.insertAdjacentHTML('beforeend', '<div class="copy-btn"><i class="fa fa-clipboard"></i></div>');
+      var button = element.parentNode.querySelector('.copy-btn');
       button.addEventListener('click', event => {
-        const target = event.currentTarget;
-        const code = [...target.parentNode.querySelectorAll('.code .line')].map(line => line.innerText).join('\n');
-        const ta = document.createElement('textarea');
+        var target = event.currentTarget;
+        var code = [...target.parentNode.querySelectorAll('.code .line')].map(line => line.innerText).join('\n');
+        var ta = document.createElement('textarea');
         ta.style.top = window.scrollY + 'px'; // Prevent page scrolling
         ta.style.position = 'absolute';
         ta.style.opacity = '0';
@@ -87,9 +83,9 @@ NexT.utils = {
         ta.select();
         ta.setSelectionRange(0, code.length);
         ta.readOnly = false;
-        const result = document.execCommand('copy');
+        var result = document.execCommand('copy');
         if (CONFIG.copycode.show_result) {
-          target.querySelector('i').className = result ? 'fa fa-check fa-fw' : 'fa fa-times fa-fw';
+          target.querySelector('i').className = result ? 'fa fa-check' : 'fa fa-times';
         }
         ta.blur(); // For iOS
         target.blur();
@@ -101,7 +97,7 @@ NexT.utils = {
       });
       button.addEventListener('mouseleave', event => {
         setTimeout(() => {
-          event.target.querySelector('i').className = 'fa fa-clipboard fa-fw';
+          event.target.querySelector('i').className = 'fa fa-clipboard';
         }, 300);
       });
     });
